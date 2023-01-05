9d6f13c156a67093d24ff03be871738bb280fae9
davidarizaldi <davidarizaldi@yahoo.com>
2023-01-05 11:45:22 +0800

    Ultraleap Implementation Fixed
    - fixed some implementation

--- Assets/Scripts/MagnetSpin.cs
+++ Assets/Scripts/MagnetSpin.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MagnetSpin : MonoBehaviour
 {
-    private Vector3 rotateChange;
+    public Vector3 rotateChange;
     // Start is called before the first frame update
     void Start()
     {
